2a07165cceaba8822703c44c372d7cbf7dea24da
Spencer Y <love4yzp@gmail.com>
2025-01-08 14:25:47 +0800

    upgrade to actions-gh-pages@v4

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = { // Front info
   url: 'https://seeed-studio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Hazard-Response-Mission-Pack',
+  baseUrl: '/Hazard-Response-Mission-Pack/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
